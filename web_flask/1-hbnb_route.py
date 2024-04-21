2e6b3783e7c7729af9806196fd2ada65dcc0dadd
itsferro <feras.mamon.alzaidi@gmail.com>
2024-04-22 01:48:27 +0200

    Task number 1 attempt number 2

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -16,12 +16,14 @@ def index():
     """
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def index():
     """
     Display 'HBNB' when accessing the root route.
     """
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
